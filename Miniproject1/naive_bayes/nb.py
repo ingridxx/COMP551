df5ad46fa75d248257f4c764b9506ab2ae208574
Ali Tapan <uluc.tapan@mail.mcgill.ca>
2020-02-09 20:08:09 -0500

    Finished NB for all datasets

--- Miniproject1/naive_bayes/nb.py
+++ Miniproject1/naive_bayes/nb.py
@@ -7,52 +7,60 @@ from sklearn.preprocessing import LabelEncoder
 
 class NaiveBayes:
 
-    # input numpy nd array, first dimension is the number of samples, second dimension is the number of features (rows = samples, cols = features)
-    # y is a 1-D row vector which is size of number of samples
+    _mean = []
+    _stdvar = []
+    _prior_probability = []
+
+    # X: numpy nd array, rows = samples, columns = features
+    # y: 1-D row vector, size of number of samples
     def fit(self, X, y):
         # Unpack
         n_samples, n_features = X.shape
 
         # Finds the number of unique elements in our output
         self._classes = np.unique(y)
+
         n_classes = len(self._classes)
         # init mean, var, priors
         self._mean = np.zeros((n_classes, n_features), dtype=np.float64)
-        self._var = np.zeros((n_classes, n_features), dtype=np.float64)
-        self._prior = np.zeros(n_classes, dtype=np.float64)
+        self._stdvar = np.zeros((n_classes, n_features), dtype=np.float64)
+        self._prior_probability = np.zeros(n_classes, dtype=np.float64)
 
         for c in self._classes:
             X_c = X[c == y]
             self._mean[c, :] = X_c.mean(axis=0)
-            self._var[c, :] = X_c.var(axis=0)
+            self._stdvar[c, :] = X_c.var(axis=0)
             # prior probability = frequency of the class / total number of samples
-            self._prior[c] = X_c.shape[0] / float(n_samples)
+            self._prior_probability[c] = X_c.shape[0] / float(n_samples)
 
     def predict(self, X):
-        y_pred = [self._predict(x) for x in X]
-        return y_pred
+        y_hat = [self.predict_single(x) for x in X]
+        return y_hat
 
     # Helper method for a single variable
-    def _predict(self, x):
+    def predict_single(self, x):
         predictions = []
 
         for idx, c in enumerate(self._classes):
-            prior = np.log(self._prior[idx])
+            prior = np.log(self._prior_probability[idx])
             class_conditional = np.sum(np.log(self._pdf(idx, x)))
             posterior = prior + class_conditional
             predictions.append(posterior)
 
         # Choose the class for the highest probability
-        return self._classes[np.argmax(posteriors)]
+        return self._classes[np.argmax(predictions)]
 
     # Probability density function (Gaussian)
     def _pdf(self, class_idx, x):
         mean = self._mean[class_idx]
-        var = self._var[class_idx]
+        var = self._stdvar[class_idx]
         try:
             denominator = np.sqrt(2 * np.pi * var)
             numarator = np.exp(- (x - mean)**2 / (2 * var))
-            return numarator / denominator
+            if np.all(numarator / denominator != 0):
+                return numarator / denominator
+            else:
+                return 1
         except RuntimeWarning:
             return 1
 
@@ -65,20 +73,45 @@ class NaiveBayes:
 warnings.filterwarnings("error")
 nb = NaiveBayes()
 
+# Ionosphere
+dataframe = Preprocess("ionosphere.data", "g")
+X = dataframe.X
+y = dataframe.y
+nb.fit(X, y)
+predictions = nb.predict(X)
+print("Accuracy in % for ionosphere dataset: ", nb.evaluate_acc(y, predictions))
 
-# dataframe = Preprocess("ionosphere.data", "g")
-# X = dataframe.X
-# y = dataframe.y
-
-
+# Adult
 dataframe = pd.read_csv("adult.data", header=None)
 dataframe = dataframe.apply(LabelEncoder().fit_transform)
 array = dataframe.values
 X = array[:, :-1]
 y = array[:, -1:]
 y = y.ravel()
-
 nb.fit(X, y)
 predictions = nb.predict(X)
+print("Accuracy in % for adult dataset: ", nb.evaluate_acc(y, predictions))
 
-print("Accuracy in %", nb.evaluate_acc(y, predictions))
+# Breast-Cancer
+dataframe = pd.read_csv("breast-cancer.data", header=None)
+dataframe = dataframe.replace(to_replace="?", value=np.nan)
+dataframe = dataframe.dropna()
+dataframe = dataframe.apply(LabelEncoder().fit_transform)
+array = dataframe.values
+X = array[:, :-1]
+y = array[:, -1:]
+y = y.ravel()
+nb.fit(X, y)
+predictions = nb.predict(X)
+print("Accuracy in % for breast-cancer dataset: ", nb.evaluate_acc(y, predictions))
+
+# Wine
+dataframe = pd.read_csv("winequality-red.csv", sep=';', header=None, skiprows=1)
+array = dataframe.values
+X = array[:, :-1]
+y = array[:, -1:]
+y = y.ravel()
+y = LabelEncoder().fit_transform(y)
+nb.fit(X, y)
+predictions = nb.predict(X)
+print("Accuracy in % for wine-quality dataset: ", nb.evaluate_acc(y, predictions))
